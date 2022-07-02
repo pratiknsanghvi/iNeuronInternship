e909c2fcada0fbf531303977c6d3f8007b777817
Pratik-Sanghvi <pratiknsaghvi89@gmail.com>
2022-07-02 11:24:38 +0530

    changes to app.run

--- app.py
+++ app.py
@@ -212,5 +212,5 @@ def predict_post_knn():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
-
+    #app.run(host='0.0.0.0', port=8000, debug=True) - run on local machine
+    app.run(debug=True)
